b745a38d56bf5a6cb2496834758a218833f4fa79
galuhlaksita <80595945+galuhlaksita@users.noreply.github.com>
2021-07-18 23:15:42 +0700

    Project Akhir

---
+++ MillenialParkir20/app/src/main/java/com/example/millenialparkir20/Barcode.java
@@ -0,0 +1,103 @@
+package com.example.millenialparkir20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.blikoon.qrcodescanner.QrCodeActivity;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class Barcode extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_QR_SCAN = 101;
+    Button scan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode);
+        scan=findViewById(R.id.scan);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dexter.withContext(getApplicationContext())
+                        .withPermission(Manifest.permission.CAMERA)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                                Intent i = new Intent(Barcode.this, QrCodeActivity.class);
+                                startActivityForResult( i,REQUEST_CODE_QR_SCAN);
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                                permissionDeniedResponse.getRequestedPermission();
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                            }
+                        }).check();
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != Activity.RESULT_OK)
+        {
+            if(data==null)
+                return;
+            //Getting the passed result
+            String result = data.getStringExtra("com.blikoon.qrcodescanner.error_decoding_image");
+            if( result!=null)
+            {
+                AlertDialog alertDialog = new AlertDialog.Builder(Barcode.this).create();
+                alertDialog.setTitle("Scan Error");
+                alertDialog.setMessage("QR Code Gagal Melakukan Scanning");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+            return;
+
+        }
+        if(requestCode == REQUEST_CODE_QR_SCAN)
+        {
+            if(data==null)
+                return;
+            //Getting the passed result
+            String result = data.getStringExtra("com.blikoon.qrcodescanner.got_qr_scan_relult");
+            AlertDialog alertDialog = new AlertDialog.Builder(Barcode.this).create();
+            alertDialog.setTitle("Hasil Scann");
+            alertDialog.setMessage(result);
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+
+        }
+    }
+
+}
---
+++ MillenialParkir20/app/src/main/java/com/example/millenialparkir20/DBContract.java
@@ -0,0 +1,6 @@
+package com.example.millenialparkir20;
+
+public class DBContract {
+    public static final String SERVER_LOGIN_URL = "http://192.168.43.239/Millenial-Parkir-System/checkLogin.php";
+    public static final String SERVER_REGISTER_URL = "http://192.168.43.239/Millenial-Parkir-System/createData.php";
+}
